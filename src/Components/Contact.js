17382230658c083d1df128ddd7613e8d2721d44c
Jonny Fluckey <jonathon.fluckey@gmail.com>
2019-05-21 12:41:51 -0600

    made contact mobile responsive

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -8,26 +8,50 @@ import GithubIcon from '../Images/Github.png';
 import ScrollAnimation from 'react-animate-on-scroll';
 import {Link} from 'react-router-dom';
 import Footer from './Shared/Footer';
+import { unstable_useMediaQuery as useMediaQuery } from '@material-ui/core/useMediaQuery';
 
 
-const HomeContainer = styled.div`
+
+
+
+export default function Contact() {
+  const screenSize = useMediaQuery('(min-width:600px)');
+
+  const HomeContainer = styled.div`
+  background-color: #AEB1BF;
+  height: 100%;
+  width: 100%;
+  padding-right: 10%;
+  padding-left: 10%;
+  position: absolute;
+  padding-bottom: 100px;
+  border-style: solid;
+  border-left-width: 75px;
+  border-left-color: #656773;
+  border-right-width: 75px;
+  border-right-color: #656773;
+  position: relative;
+  `
+
+  const HomeMobileContainer = styled.div`
       background-color: #AEB1BF;
-      height: 100%;
-      width: 100%;
+      height: ;
+      width: ;
       padding-right: 10%;
       padding-left: 10%;
       position: absolute;
       padding-bottom: 100px;
       border-style: solid;
-      border-left-width: 75px;
+      border-left-width: 25px;
       border-left-color: #656773;
-      border-right-width: 75px;
+      border-right-width: 25px;
       border-right-color: #656773;
       position: relative;
       `
 
-const Contact= () => (
+if (screenSize == true) {
 
+  return (
 <>
 <HomeContainer>
   <Typography variant='h2' align='center' style={{marginTop: '100px'}}>Contact Jonny through the following channels:</Typography>
@@ -40,7 +64,7 @@ const Contact= () => (
       justify='center'
       alignItems='center'
       >
-  <Grid item xs={3} align='center'>
+  <Grid item xs={6} sm={3} align='center'>
   <ScrollAnimation animateIn='rollIn'>
   <a href='///www.facebook.com/jfluckey1' target='_blank'>
   <img src={FBIcon} height='50px' width='50px'/>
@@ -48,7 +72,7 @@ const Contact= () => (
   <Typography variant='body1'>Jonathon Fluckey</Typography>
   </ScrollAnimation>
   </Grid>
-  <Grid item xs={3} align='center'>
+  <Grid item xs={6} sm={3} align='center'>
   <ScrollAnimation animateIn='rollIn'>
   <a href='///www.twitter.com/jonnyfluckey' target='_blank'>
   <img src={TwitterIcon} height='50px' width='50px'/>
@@ -56,7 +80,7 @@ const Contact= () => (
   <Typography variant='body1'>@jonnyfluckey</Typography>
   </ScrollAnimation>
   </Grid>
-  <Grid item xs={3} align='center'>
+  <Grid item xs={6} sm={3} align='center'>
   <ScrollAnimation animateIn='rollIn'>
   <a href='///www.linkedin.com/in/jonathonfluckey' target='_blank'>
   <img src={LinkedInIcon} height='50px' width='50px' />
@@ -64,7 +88,7 @@ const Contact= () => (
   <Typography variant='body1'>Jonathon Fluckey</Typography>
   </ScrollAnimation>
   </Grid>
-  <Grid item xs={3} align='center'>
+  <Grid item xs={6} sm={3} align='center'>
   <ScrollAnimation animateIn='rollIn'>
   <a href='///www.github.com/jonnyfluckey' target='_blank'>
   <img src={GithubIcon} height='50px' width='50px' style={{backgroundColor: 'white', borderRadius: '100%'}}/>
@@ -77,7 +101,56 @@ const Contact= () => (
 </HomeContainer>
 <Footer />
 </>
-
-)
-
-export default Contact
+)} else {
+  return (
+    <>
+    <HomeMobileContainer>
+    <Typography variant='h2' align='center' style={{marginTop: '100px'}}>Contact Jonny through the following channels:</Typography>
+  <br></br>
+  <br></br>
+  <Grid style={{marginTop: '100px'}} 
+      container 
+      spacing={24}
+      alignContent='center'
+      justify='center'
+      alignItems='center'
+      >
+  <Grid item xs={6} sm={3} align='center'>
+  <ScrollAnimation animateIn='rollIn'>
+  <a href='///www.facebook.com/jfluckey1' target='_blank'>
+  <img src={FBIcon} height='50px' width='50px'/>
+  </a>
+  <Typography variant='body1'>Jonathon Fluckey</Typography>
+  </ScrollAnimation>
+  </Grid>
+  <Grid item xs={6} sm={3} align='center'>
+  <ScrollAnimation animateIn='rollIn'>
+  <a href='///www.twitter.com/jonnyfluckey' target='_blank'>
+  <img src={TwitterIcon} height='50px' width='50px'/>
+  </a>
+  <Typography variant='body1'>@jonnyfluckey</Typography>
+  </ScrollAnimation>
+  </Grid>
+  <Grid item xs={6} sm={3} align='center'>
+  <ScrollAnimation animateIn='rollIn'>
+  <a href='///www.linkedin.com/in/jonathonfluckey' target='_blank'>
+  <img src={LinkedInIcon} height='50px' width='50px' />
+  </a>
+  <Typography variant='body1'>Jonathon Fluckey</Typography>
+  </ScrollAnimation>
+  </Grid>
+  <Grid item xs={6} sm={3} align='center'>
+  <ScrollAnimation animateIn='rollIn'>
+  <a href='///www.github.com/jonnyfluckey' target='_blank'>
+  <img src={GithubIcon} height='50px' width='50px' style={{backgroundColor: 'white', borderRadius: '100%'}}/>
+  </a>
+  <Typography variant='body1'>jonnyfluckey</Typography>
+  </ScrollAnimation>
+  </Grid>
+  </Grid>
+    </HomeMobileContainer>
+    <Footer />
+    </>
+  )
+}
+}
