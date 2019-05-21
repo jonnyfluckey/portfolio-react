10e852cb914deb1637eb8fcff372b34d3d36a8df
Jonny Fluckey <jonathon.fluckey@gmail.com>
2019-05-21 08:34:21 -0600

    splash page mobile responsive

--- src/Components/Splash.js
+++ src/Components/Splash.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import { Typography, Button, Grid, GridList } from '@material-ui/core';
+import { Typography, Button, GridList } from '@material-ui/core';
 import styled from 'styled-components';
 import Image from '../Images/JFProfile.jpg';
 import ScrollAnimation from 'react-animate-on-scroll';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import { unstable_useMediaQuery as useMediaQuery } from '@material-ui/core/useMediaQuery';
 
 
-class Splash extends React.Component {
+export default function Splash() {
+  const screenSize = useMediaQuery('(min-width:600px)');
+
+
+// class Splash extends React.Component {
 
 
   
 
-  render() {
+//   render() {
 
 
     const SplashContainer = styled.div`
@@ -20,7 +25,7 @@ class Splash extends React.Component {
       width: 100%;
       position: absolute;
       `
-
+    if (screenSize == true) {
     return (
       <SplashContainer> 
       <img src={Image} height='200px' width='200px' style={{float: 'left', marginTop: '13%', borderRadius: '50%', marginLeft: '25%'}} />
@@ -32,11 +37,21 @@ class Splash extends React.Component {
         </Link>
         </ScrollAnimation>
       </GridList>
-        
-        
       </SplashContainer>
-    )
+    )} else {
+      return (
+        <SplashContainer> 
+      <img src={Image} height='200px' width='200px' style={{marginTop: '13%', borderRadius: '50%', marginLeft: '25%'}} />
+      <GridList cols={1} style={{marginTop: '15%'}} cellHeight='100px'>
+        <ScrollAnimation animateIn='lightSpeedIn'>
+        <Typography variant="h3" direction='row' align='center'>Hi, I'm Jonny Fluckey</Typography>
+        <Link to='/home' style={{textDecoration: 'none'}}>
+        <Button variant="outlined" color="primary" size="large" style={{width: '200px', marginTop: '25px', marginLeft: '25%'}}>Learn More</Button>
+        </Link>
+        </ScrollAnimation>
+      </GridList>
+      </SplashContainer>
+      )
+    }
   }
-}
-
-export default Splash
+// export default Splash
